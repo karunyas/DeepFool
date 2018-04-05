481b74c1c0724309e0de00f0b7c4051a9aa66aaa
lilyweng <twweng@mit.edu>
2018-04-04 20:43:26 -0400

    avoid _rebuild_tensor_v2 error in the latest pytorch

--- Lily/train_2layer.py
+++ Lily/train_2layer.py
@@ -19,6 +19,20 @@ from torchvision import datasets, transforms
 from torch.autograd import Variable
 
 
+# Monkey-patch because I trained with a newer version.
+# This can be removed once PyTorch 0.4.x is out.
+# See https://discuss.pytorch.org/t/question-about-rebuild-tensor-v2/14560
+import torch._utils
+try:
+    torch._utils._rebuild_tensor_v2
+except AttributeError:
+    def _rebuild_tensor_v2(storage, storage_offset, size, stride, requires_grad, backward_hooks):
+        tensor = torch._utils._rebuild_tensor(storage, storage_offset, size, stride)
+        tensor.requires_grad = requires_grad
+        tensor._backward_hooks = backward_hooks
+        return tensor
+    torch._utils._rebuild_tensor_v2 = _rebuild_tensor_v2
+
 
 ## define nn model
 class Net(nn.Module):
